003b5734db48d0e8899ecc3977a62dea5b75673a
jykang <junyoung.kang@navercorp.com>
2020-05-12 18:07:07 +0900

    first commit

---
+++ src/config.js
@@ -0,0 +1,29 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+
+  // Your Github UserName
+  githubUserName: "kgneng2",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "blokg",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.27452921632a23cb',
+  OAuthSecret: 'd46e20f2b42349ca46780c6bfc397d9121f4e33d',
+
+  // Your Personal Blog Title
+  title : "kgneng2 blog" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    titleColor: '#ff5252',
+    subtitleColor: '#37474f',
+  },
+};
